6d289e107a4013f8fb9b1752b4d144d8fd364612
Lars Thorup <lars@zealake.com>
2022-10-07 11:47:42 +0200

    extracting load dependencies

--- log-loader.js
+++ log-loader.js
@@ -1,9 +1,31 @@
-export async function load(url, context, nextLoad) {
+export async function resolve(specifier, context, nextResolve) {
+  const { parentURL: parent } = context;
+  const { format, shortCircuit, url } = await nextResolve(specifier, context);
+  const urlWithParent = new URL(url);
+  if (format === "module" && parent) {
+    const parentURL = new URL(parent);
+    parentURL.search = ""; // Note: strip grandparent
+    urlWithParent.search = `?${new URLSearchParams({
+      parent: parentURL.href,
+    })}`;
+  }
+  return {
+    format,
+    shortCircuit: true,
+    url: urlWithParent.toString(),
+  };
+}
+
+export async function load(urlString, context, nextLoad) {
   const { format } = context;
   if (format === "module") {
-    const { source: originalSource } = await nextLoad(url, context);
-    const transformedSource = `${originalSource};\nconsole.log('loaded ${url}');`;
+    const url = new URL(urlString);
+    const moduleUrl = new URL(url);
+    const parent = moduleUrl.searchParams.get("parent");
+    moduleUrl.search = "";
+    const { source: originalSource } = await nextLoad(moduleUrl.href, context);
+    const transformedSource = `${originalSource};\nconsole.log('loaded ${moduleUrl.href} from ${parent}');`;
     return { format: "module", source: transformedSource, shortCircuit: true };
   }
-  return nextLoad(url);
+  return nextLoad(urlString);
 }
